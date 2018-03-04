3394d6342b46ca659653562ef8bff469a866aec4
Özgür Özçıtak <oozcitak@outlook.com>
2018-03-05 01:49:47 +0300

    Point2D control points can be automatically read by name.

--- SimpleCAD/ControlPoint.cs
+++ SimpleCAD/ControlPoint.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,15 +22,24 @@ namespace SimpleCAD
         public ControlPointType Type { get; private set; }
         public Point2D BasePoint { get; private set; }
         public Point2D Location { get; private set; }
+        private bool pointSet;
+
+        public ControlPoint(string propertyName)
+            : this(propertyName, -1, ControlPointType.Point, Point2D.Zero, Point2D.Zero)
+        {
+            pointSet = false;
+        }
+
+        public ControlPoint(string propertyName, int propertyIndex)
+            : this(propertyName, propertyIndex, ControlPointType.Point, Point2D.Zero, Point2D.Zero)
+        {
+            pointSet = false;
+        }
 
         public ControlPoint(string propertyName, ControlPointType type, Point2D basePoint, Point2D location)
+            : this(propertyName, -1, type, basePoint, location)
         {
-            Owner = null;
-            PropertyName = propertyName;
-            PropertyIndex = -1;
-            Type = type;
-            BasePoint = basePoint;
-            Location = location;
+            ;
         }
 
         public ControlPoint(string propertyName, int propertyIndex, ControlPointType type, Point2D basePoint, Point2D location)
@@ -40,13 +50,35 @@ namespace SimpleCAD
             Type = type;
             BasePoint = basePoint;
             Location = location;
+            pointSet = false;
         }
 
         internal static ControlPoint[] FromDrawable(Drawable item, float size)
         {
             ControlPoint[] points = item.GetControlPoints(size);
             for (int i = 0; i < points.Length; i++)
-                points[i].Owner = item;
+            {
+                ControlPoint cp = points[i];
+                cp.Owner = item;
+                if (!cp.pointSet)
+                {
+                    // Read the point location with reflection
+                    PropertyInfo prop = item.GetType().GetProperty(points[i].PropertyName);
+                    Point2D pt;
+                    if (cp.PropertyIndex == -1)
+                    {
+                        pt = (Point2D)prop.GetValue(item);
+                    }
+                    else
+                    {
+                        IList<Point2D> itemPoints = (IList<Point2D>)prop.GetValue(item);
+                        pt = itemPoints[cp.PropertyIndex];
+                    }
+                    cp.BasePoint = pt;
+                    cp.Location = pt;
+                    cp.pointSet = true;
+                }
+            }
             return points;
         }
     }
