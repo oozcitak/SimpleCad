408fb051c399a0050728e3bde03a6b56e9a057cf
Özgür Özçıtak <oozcitak@outlook.com>
2019-10-15 04:23:52 +0300

    Better color parser

--- SimpleCAD/Graphics/ColorConverter.cs
+++ SimpleCAD/Graphics/ColorConverter.cs
@@ -41,7 +41,38 @@ namespace SimpleCAD.Graphics
                     str = str.Substring(1);
 
                 if (uint.TryParse(str, NumberStyles.HexNumber, CultureInfo.CurrentCulture, out uint argb))
-                    return Color.FromArgb(argb);
+                {
+                    byte a = 0xff;
+                    byte r = 0;
+                    byte g = 0;
+                    byte b = 0;
+                    if (str.Length > 8) str = str.Substring(0, 8);
+                    if (str.Length > 6)
+                    {
+                        a = byte.Parse(str.Substring(0, str.Length - 6), NumberStyles.HexNumber, CultureInfo.CurrentCulture);
+                        r = byte.Parse(str.Substring(str.Length - 6, 2), NumberStyles.HexNumber, CultureInfo.CurrentCulture);
+                        g = byte.Parse(str.Substring(str.Length - 4, 2), NumberStyles.HexNumber, CultureInfo.CurrentCulture);
+                        b = byte.Parse(str.Substring(str.Length - 2, 2), NumberStyles.HexNumber, CultureInfo.CurrentCulture);
+                    }
+                    else if (str.Length > 4)
+                    {
+                        r = byte.Parse(str.Substring(0, str.Length - 4), NumberStyles.HexNumber, CultureInfo.CurrentCulture);
+                        g = byte.Parse(str.Substring(str.Length - 4, 2), NumberStyles.HexNumber, CultureInfo.CurrentCulture);
+                        b = byte.Parse(str.Substring(str.Length - 2, 2), NumberStyles.HexNumber, CultureInfo.CurrentCulture);
+                    }
+                    else if (str.Length > 2)
+                    {
+                        r = byte.Parse(str.Substring(0, str.Length - 2), NumberStyles.HexNumber, CultureInfo.CurrentCulture);
+                        g = byte.Parse(str.Substring(str.Length - 2, 1), NumberStyles.HexNumber, CultureInfo.CurrentCulture);
+                        b = byte.Parse(str.Substring(str.Length - 1, 1), NumberStyles.HexNumber, CultureInfo.CurrentCulture);
+                        str = str.Substring(str.Length - 2);
+                    }
+                    else
+                    {
+                        r = byte.Parse(str, NumberStyles.HexNumber, CultureInfo.CurrentCulture);
+                    }
+                    return Color.FromArgb(a, r, g, b);
+                }
 
                 if (Enum.TryParse(str, out KnownColor knownColor))
                     return Color.FromKnownColor(knownColor);
